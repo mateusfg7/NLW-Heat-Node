5f4e7747800eb6e83f0eded2a8873101659d660c
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-10-18 20:55:24 -0300

    new route
    - `/signin/callback`

--- src/app.ts
+++ src/app.ts
@@ -7,4 +7,10 @@ app.get("/github", (request, response) => {
   response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
 })
 
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query
+
+  return response.json(code)
+})
+
 app.listen(4000, () => console.log(':rocket: Server is running on PORT 4000'))
