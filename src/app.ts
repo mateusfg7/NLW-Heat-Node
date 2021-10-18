c7cef5d6a6caa0fe99f8d28d10bcdc02445212c8
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-10-18 20:52:35 -0300

    new route
    - `/github`

--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,8 @@ import express from 'express'
 
 const app = express()
 
+app.get("/github", (request, response) => {
+  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
 
 app.listen(4000, () => console.log(':rocket: Server is running on PORT 4000'))
