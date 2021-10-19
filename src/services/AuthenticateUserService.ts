73e7366a9cc210b0ead11769b3f8062aac4b3e68
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-10-18 22:35:51 -0300

    update Authenticate User Service
    - generate a token for user

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { sign } from 'jsonwebtoken'
 import prismaClient from '../prisma'
 
 /**
@@ -16,9 +17,9 @@ interface IAccessTokenResponse {
 }
 
 interface IUserResponse {
-  avatar_url: string,
-  login: string,
-  id: number,
+  avatar_url: string
+  login: string
+  id: number
   name: string
 }
 
@@ -26,29 +27,33 @@ class AuthenticateUserService {
   async execute(code: string) {
     const url = 'https://github.com/login/oauth/access_token'
 
-    const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
-      params: {
-        client_id: process.env.GITHUB_CLIENT_ID,
-        client_secret: process.env.GITHUB_CLIENT_SECRET,
-        code
-      },
-      headers: {
-        "Accept": "application/json"
-      }
-    })
+    const { data: accessTokenResponse } =
+      await axios.post<IAccessTokenResponse>(url, null, {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: 'application/json',
+        },
+      })
 
-    const response = await axios.get<IUserResponse>("https://api.github.com/user", {
-      headers: {
-        authorization: `Bearer ${accessTokenResponse.access_token}`
+    const response = await axios.get<IUserResponse>(
+      'https://api.github.com/user',
+      {
+        headers: {
+          authorization: `Bearer ${accessTokenResponse.access_token}`,
+        },
       }
-    })
+    )
 
     const { login, id, avatar_url, name } = response.data
 
     let user = await prismaClient.user.findFirst({
       where: {
-        github_id: id
-      }
+        github_id: id,
+      },
     })
 
     if (!user) {
@@ -57,12 +62,27 @@ class AuthenticateUserService {
           github_id: id,
           login,
           avatar_url,
-          name
-        }
+          name,
+        },
       })
     }
 
-    return response.data
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          id: user.id,
+        },
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: '1d',
+      }
+    )
+
+    return { token, user }
   }
 }
 
