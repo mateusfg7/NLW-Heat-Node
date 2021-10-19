9b80433dbd14ec9ce43e2f28ea691d2de8b5dca7
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-10-18 22:00:01 -0300

    update Authenticate User Service
    - get access token

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -2,18 +2,23 @@ import axios from 'axios'
 
 /**
  * X Receber code(string)
- * Recuperar o access_token no github
+ * X Recuperar o access_token no github
+ * Recuperar infos do user no github
  * Verificar se o usuário existe no banco de dados
  * --- SIM = Gera um token
  * --- NÃO = Cria no DB, gera um token
  * Retornar o token com as infos do user
  */
 
+interface IAccessTokenResponse {
+  access_token: string
+}
+
 class AuthenticateUserService {
   async execute(code: string) {
     const url = 'https://github.com/login/oauth/access_token'
 
-    const response = await axios.post(url, null, {
+    const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
       params: {
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -24,7 +29,7 @@ class AuthenticateUserService {
       }
     })
 
-    return response.data
+    return accessTokenResponse.access_token
   }
 }
 
